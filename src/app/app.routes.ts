cdb66315b28725bc0c1047c60cd99e36c7e7ad77
aziz ltaief <ltaief2022@gmail.com>
2025-05-04 14:26:08 +0100

    removing wrong project files

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router'; // Keep only one import
-
-import { FragranceDetailComponent } from './pages/fragrance-detail/fragrance-detail.component';
-
-export const routes: Routes = [  // Make sure 'routes' is exported
-  { path: 'fragrance/:id', component: FragranceDetailComponent },
-  { path: '', redirectTo: '/fragrance/1', pathMatch: 'full' } // Default page
-];
